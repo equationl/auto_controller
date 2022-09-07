8e85c3bae3c18277fead58064cb962bd4c05e02f
equationl <equationl@qq.com>
2022-09-07 13:31:47 +0800

    删除无用代码

--- app/src/main/java/com/equationl/autocontroller/utils/BtHelper.kt
+++ app/src/main/java/com/equationl/autocontroller/utils/BtHelper.kt
@@ -8,8 +8,6 @@ import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
 import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import kotlinx.coroutines.Dispatchers
@@ -20,16 +18,11 @@ import java.io.OutputStream
 import java.util.*
 
 class BtHelper {
-    var socket: BluetoothSocket? = null
-
     private var bluetoothAdapter: BluetoothAdapter? = null
     private var keepReceive: Boolean = true
 
     companion object {
         private const val TAG = "BtHelper"
-        const val MESSAGE_READ: Int = 0
-        const val MESSAGE_WRITE: Int = 1
-        const val MESSAGE_TOAST: Int = 2
 
         val instance by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
             BtHelper()
@@ -67,7 +60,7 @@ class BtHelper {
 
         pairedDevices?.forEach { device ->
             val deviceName = device.name
-            val deviceHardwareAddress = device.address // MAC address
+            val deviceHardwareAddress = device.address
 
             Log.i(TAG, "queryPairDevices: deveice name=$deviceName, mac=$deviceHardwareAddress")
         }
@@ -90,7 +83,6 @@ class BtHelper {
                 mmSocket?.connect()
             }.fold({
                 withContext(Dispatchers.Main) {
-                    socket = mmSocket
                     onConnected(Result.success(mmSocket!!))
                 }
             }, {
@@ -159,72 +151,4 @@ class BtHelper {
             }
         }
     }
-
-    class MyBluetoothService(
-        // handler that gets info from Bluetooth service
-        private val handler: Handler
-    ) {
-
-        private inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
-
-            private val mmInStream: InputStream = mmSocket.inputStream
-            private val mmOutStream: OutputStream = mmSocket.outputStream
-            private val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
-
-            override fun run() {
-                var numBytes: Int // bytes returned from read()
-
-                // Keep listening to the InputStream until an exception occurs.
-                while (true) {
-                    // Read from the InputStream.
-                    numBytes = try {
-                        mmInStream.read(mmBuffer)
-                    } catch (e: IOException) {
-                        Log.d(TAG, "Input stream was disconnected", e)
-                        break
-                    }
-
-                    // Send the obtained bytes to the UI activity.
-                    val readMsg = handler.obtainMessage(
-                        MESSAGE_READ, numBytes, -1,
-                        mmBuffer)
-                    readMsg.sendToTarget()
-                }
-            }
-
-            // Call this from the main activity to send data to the remote device.
-            fun write(bytes: ByteArray) {
-                try {
-                    mmOutStream.write(bytes)
-                } catch (e: IOException) {
-                    Log.e(TAG, "Error occurred when sending data", e)
-
-                    // Send a failure message back to the activity.
-                    val writeErrorMsg = handler.obtainMessage(MESSAGE_TOAST)
-                    val bundle = Bundle().apply {
-                        putString("toast", "Couldn't send data to the other device")
-                    }
-                    writeErrorMsg.data = bundle
-                    handler.sendMessage(writeErrorMsg)
-                    return
-                }
-
-                // Share the sent message with the UI activity.
-                val writtenMsg = handler.obtainMessage(
-                    MESSAGE_WRITE, -1, -1, mmBuffer)
-                writtenMsg.sendToTarget()
-            }
-
-            // Call this method from the main activity to shut down the connection.
-            fun cancel() {
-                try {
-                    mmSocket.close()
-                } catch (e: IOException) {
-                    Log.e(TAG, "Could not close the connect socket", e)
-                }
-            }
-        }
-    }
-
-
 }
